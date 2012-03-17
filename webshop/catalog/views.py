1b9ba095d26b582ef141ddc0cb84e1aff53bcb3e
Andrey Selemenev <andreyjkee@gmail.com>
2012-03-17 19:40:58 +0400

    Пробный коммит из PyCharm Рефакторинг

--- webshop/catalog/views.py
+++ webshop/catalog/views.py
@@ -14,8 +14,7 @@ def index(request, template_name="catalog/index.html"):
 	return render_to_response(template_name, locals(), 
 		context_instance=RequestContext(request))
 	
-def show_category(request, category_slug, 
-	template_name="catalog/category.html"):
+def show_category(request, category_slug, template_name="catalog/category.html"):
 	"""Представление для просмотра конкретной категории"""
 	c = get_object_or_404(Category, slug=category_slug)
 	products = c.product_set.all()
