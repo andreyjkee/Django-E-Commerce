9ecf012e90d5cf7c8ba41c43ef2478af15f74db6
Andrey Selemenev <andreyjkee@gmail.com>
2012-03-30 21:40:17 +0400

    Удален не используемый импорт

--- webshop/cart/views.py
+++ webshop/cart/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_protect
