532ceacc956a4674c9904079baeb5a99b4f999ce
Andrey Selemenev <andreyjkee@gmail.com>
2012-03-30 21:42:15 +0400

    Подключен модуль mptt

--- webshop/settings.py
+++ webshop/settings.py
@@ -161,6 +161,8 @@ INSTALLED_APPS = (
 	'django.contrib.admin',
 	# Uncomment the next line to enable admin documentation:
 	# 'django.contrib.admindocs',
+	# Custom modules
+	'mptt',
 )
 
 # A sample logging configuration. The only tangible logging
