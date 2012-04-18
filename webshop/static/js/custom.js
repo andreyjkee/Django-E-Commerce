dce4ee23c231dbb5c0927e58530ab49fc263688b
Andrey Selemenev <andreyjkee@gmail.com>
2012-04-18 21:24:53 +0400

    js скрипты

---
+++ webshop/static/js/custom.js
@@ -0,0 +1,15 @@
+function highlightActiveTab() {
+    // Получаем адрес текущей страницы
+    var url = window.location.pathname;
+    // проверяем главная это страница или нет
+    if (url != '/') {
+        urlRegExp = new RegExp(url.replace(/\/$/, ''));
+        $('.nav a').each(function () {
+            if (urlRegExp.test(this.href)) {
+                $(this).parent().addClass('active');
+            }
+        });
+    }
+}
+
+$(document).ready(highlightActiveTab);
