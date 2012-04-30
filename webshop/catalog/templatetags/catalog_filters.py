32cb2263db4b8de72645922e5672153704e0f3ff
Andrey Selemenev <andreyjkee@gmail.com>
2012-04-30 20:11:25 +0400

    fix for pylint

--- webshop/catalog/templatetags/catalog_filters.py
+++ webshop/catalog/templatetags/catalog_filters.py
@@ -15,7 +15,7 @@ def currency(value):
 	"""
 	try:
 		locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
-	except:
+	except Exception:
 		locale.setlocale(locale.LC_ALL, '')
 	loc = locale.localeconv()
 	return locale.currency(value, loc['currency_symbol'], grouping=True)
