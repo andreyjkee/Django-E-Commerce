36e5fbff1cb6063305d2a9d4c0f48e65fed06725
Andrey Selemenev <andreyjkee@gmail.com>
2012-05-01 16:12:23 +0400

    Представление для страницы аккаунта

--- webshop/accounts/views.py
+++ webshop/accounts/views.py
@@ -5,38 +5,47 @@ from django.contrib.auth import login, authenticate
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from django.core import urlresolvers
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_protect
 
+#from webshop.checkout.models import Order, OrderItem
+
 @csrf_protect
 def register_view(request, template_name="registration/login.html"):
-	"""Регистрация нового пользователя"""
-	page_title = _(u'User Registration')
-	if request.method == 'POST':
-		postdata = request.POST.copy()
-		form = UserCreationForm(postdata)
-		if form.is_valid():
-			form.save()
-			un = postdata.get('username','')
-			pw = postdata.get('password','')
-			new_user = authenticate(username=un, password=pw)
-			if new_user and new_user.is_active:
-				login(request, new_user)
-				# Редирект на url с именем my_account
-				url = urlresolvers.reverse('my_account')
-				return HttpResponseRedirect(url)
-	else:
-		form = UserCreationForm()
+    """Регистрация нового пользователя"""
+    page_title = _(u'User Registration')
+    if request.method == 'POST':
+        postdata = request.POST.copy()
+        form = UserCreationForm(postdata)
+        if form.is_valid():
+            form.save()
+            un = postdata.get('username', '')
+            pw = postdata.get('password', '')
+            new_user = authenticate(username=un, password=pw)
+            if new_user and new_user.is_active:
+                login(request, new_user)
+                # Редирект на url с именем my_account
+                url = urlresolvers.reverse('my_account')
+                return HttpResponseRedirect(url)
+    else:
+        form = UserCreationForm()
 
-	return render_to_response(template_name, locals(),
-		context_instance=RequestContext(request))
+    return render_to_response(template_name, locals(),
+                              context_instance=RequestContext(request))
 
-def my_account_view():
-	pass
+@login_required
+def my_account_view(request, template_name="registration/my_account.html"):
+    """Страница аккаунта пользователя"""
+    page_title = _(u'My Account')
+#    orders = Order.objects.filter(user=request.user)
+    name = request.user.username
+    return render_to_response(template_name, locals(),
+                              context_instance=RequestContext(request))
 
 def order_details_view():
-	pass
+    pass
 
 def order_info_view():
-	pass
+    pass
