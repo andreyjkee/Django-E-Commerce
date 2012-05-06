65e0ac25386b4f5fdfc8bf1be72091a828607436
Andrey Selemenev <andreyjkee@gmail.com>
2012-05-06 21:05:07 +0400

    update

--- webshop/urls.py
+++ webshop/urls.py
@@ -5,35 +5,37 @@ from django.contrib import admin
 
 from webshop import settings
 
+
 admin.autodiscover()
 
-
 urlpatterns = patterns('',
-	# Examples:
-	# url(r'^webshop/', include('webshop.foo.urls')),
+    # Examples:
+    # url(r'^webshop/', include('webshop.foo.urls')),
 
-	# Uncomment the admin/doc line below to enable admin documentation:
-	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-	# Uncomment the next line to enable the admin:
-	url(r'^admin/', include(admin.site.urls)),
-	# General application URLs
-	url(r'^accounts/', include('django.contrib.auth.urls')),
-	url(r'^accounts/', include('webshop.accounts.urls')),
-	url(r'^', include('webshop.catalog.urls')),
-	url(r'^cart/', include('webshop.cart.urls')),
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    # General application URLs
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/', include('webshop.accounts.urls')),
+    url(r'^', include('webshop.catalog.urls')),
+    url(r'^cart/', include('webshop.cart.urls')),
 
-	# enable language choice
-	url(r'^i18n/', include('django.conf.urls.i18n')),
-	url(r'^set_language/$', 'django.views.i18n.set_language', name='set_language'),
+    # enable language choice
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^set_language/$', 'django.views.i18n.set_language', name='set_language'),
 
-)
+    )
 
 if settings.DEBUG:
-	urlpatterns += patterns('',
-		url(r'^static/(?P<path>.*)$', 'django.views.static.serve', 
-		   {'document_root' : settings.STATIC_ROOT }),
-		# if work then show 404 Directory indexes are not allowed here.
-	)
+    urlpatterns += patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT}),
+        # if work then show 404 Directory indexes are not allowed here.
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
+        )
 
 handler404 = 'webshop.views.file_not_found_404'
