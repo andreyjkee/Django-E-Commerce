cb6cc346b8a6488821660790a8c69606ce6118f5
Andrey Selemenev <andreyjkee@gmail.com>
2012-05-12 21:29:33 +0400

    Added fixtures generation for new apps

--- utils/create_fixtures.bat
+++ utils/create_fixtures.bat
@@ -1,3 +1,8 @@
 cd ..
 python manage.py dumpdata catalog --indent=2 > webshop/catalog/fixtures/initial_data.json
+python manage.py dumpdata accounts --indent=2 > webshop/accounts/fixtures/initial_data.json
+python manage.py dumpdata cart --indent=2 > webshop/cart/fixtures/initial_data.json
+python manage.py dumpdata checkout --indent=2 > webshop/checkout/fixtures/initial_data.json
+python manage.py dumpdata news --indent=2 > webshop/news/fixtures/initial_data.json
+python manage.py dumpdata search --indent=2 > webshop/search/fixtures/initial_data.json
 PAUSE
