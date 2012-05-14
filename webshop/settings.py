7bb84642da1ec008dac2a91d60037c7bed2f41b9
Andrey Selemenev <andreyjkee@gmail.com>
2012-05-14 21:14:30 +0400

    refactoring

--- webshop/settings.py
+++ webshop/settings.py
@@ -44,6 +44,7 @@ TIME_ZONE = 'Europe/Moscow'
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'ru'
 
 ugettext = lambda s: s
 
