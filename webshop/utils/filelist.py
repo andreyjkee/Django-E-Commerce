b6d3ee66d105f7e91736b42e760e6697b2b1c2fa
Andrey Selemenev <andreyjkee@gmail.com>
2012-06-02 18:08:02 +0400

    added utils for collect code

---
+++ webshop/utils/filelist.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+import os
+import fileinput
+
+
+CURRENT_PATH = os.path.abspath(os.path.dirname(__file__).decode('utf-8')).replace('\\', '/')
+EXTENSIONS = ('py', )
+FILE_NAME_TO_WRITE = u'123.txt'
+
+
+def get_files_by_extension():
+	"""Scan files in CURRENT_PATH dir """
+	files = []
+	for filename in os.listdir(CURRENT_PATH):
+		try:
+			basename, extension = filename.split('.')
+			if extension in EXTENSIONS:
+				files.append(filename)
+		except ValueError:
+			pass
+	if FILE_NAME_TO_WRITE in files:
+		files.pop(files.index(FILE_NAME_TO_WRITE))
+
+	EXCLUDE = ('__init__.py', 'tests.py', __file__)
+	for file_name in EXCLUDE:
+		try:
+			files.pop(files.index(file_name))
+		except ValueError:
+			pass
+
+	return files
+
+print get_files_by_extension()
+READED_FILES = get_files_by_extension()
+
+with open(FILE_NAME_TO_WRITE, 'a+b') as header:
+		header.write('\n\r' + u'Листинг программных модулей приложения ' + \
+			os.path.basename(os.getcwd()) + '\n\r')
+
+for file_name in READED_FILES:
+	with open(FILE_NAME_TO_WRITE, 'a+b') as header:
+		header.write('\n\r' + u'Листинг модуля ' + file_name + '\n\r')
+	for line in fileinput.input(file_name):
+		with open(FILE_NAME_TO_WRITE, 'a+b') as result:
+			result.write(line)
+
